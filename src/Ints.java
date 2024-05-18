65a80be69dc85d5ded8e82c323b5ce32258cc5ed
JavaJedi <aleksk170400@gmail.com>
2024-05-19 03:33:06 +0500

    Initial commit

--- src/Ints.java
+++ src/Ints.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public interface Ints {
+public interface Ints {
+    int sum(int arg0, int arg1);
+
+    int mult(int arg0, int arg1);
+
+    int pow(int a, int b);
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.sum(5, 10));
+        System.out.println(calc.mult(3, 17));
+        System.out.println(calc.pow(6, 32));
+        //демонстрация
+    }
 }
